f9bb1dd483c3c107484dca5af1fb8b4c3d75fe79
Naieem Mahmud Supto <naieemsupto@gmail.com>
2020-04-25 17:23:43 +0600

    unused code removed

--- js/project-js.js
+++ js/project-js.js
@@ -1,5 +1,5 @@
 
-// on ready device ackowledgement gatherer
+// on ready device ackowledgement cordova
 (function () {
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
@@ -31,19 +31,6 @@
             // alert("mal ready done");
         }
     };
-})();
-// bottom nav click event listener
-(function () {
-    // var navItems = document.querySelectorAll(".mobile-bottom-nav__item");
-    // debugger;
-    // navItems.forEach(function (e, i) {
-    //     e.addEventListener("click", function (e) {
-    //         navItems.forEach(function (e2, i2) {
-    //             e2.classList.remove("mobile-bottom-nav__item--active");
-    //         })
-    //         this.classList.add("mobile-bottom-nav__item--active");
-    //     });
-    // });
 })();
 
 // vue initializer
