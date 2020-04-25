0d48e09a6078864826ea3b16e07b6a59914e5135
Naieem Mahmud Supto <naieemsupto@gmail.com>
2020-04-25 17:09:51 +0600

    router added succesfully and tarabih rules added

---
+++ js/components/rules.component.js
@@ -0,0 +1,9 @@
+window.rulesComponent = Vue.component('button-counter', {
+    data: function () {
+        return {
+        }
+    },
+    methods: {
+    },
+    template: '#rules-template',
+});
